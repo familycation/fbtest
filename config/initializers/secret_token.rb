ab1d63dd51a0a8085584cb481f3e66ef98878bd2
familycation <familycation@gmail.com>
2013-12-04 22:04:23 +0900

    Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fbtest::Application.config.secret_key_base = 'cfd0e758cabd2c3c67b4fbd4b8269681971cfa503f59a22b3f0e57ceabfdde9d32f463831f88cf9932a31e044ea7a1b229cf827adec9859234b67ec9a0e29d60'
